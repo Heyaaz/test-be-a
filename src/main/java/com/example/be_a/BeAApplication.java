fc80315040b461e8dc749006f1b1c747be2667f8
Heeyaa <pion0458@gmail.com>
2026-04-22 13:31:19 +0900

    chore: BE-A 모듈 스캐폴딩 추가

---
+++ src/main/java/com/example/be_a/BeAApplication.java
@@ -0,0 +1,12 @@
+package com.example.be_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeAApplication.class, args);
+    }
+}
